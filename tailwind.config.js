7aa4d509dfbad81a915102bb3460ee8563388602
berhanu <berhanu@metactical.com>
2024-03-23 05:41:14 +0300

    Add mobile hambergure menu and more

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        headerwidth: '84.65%',
+        mainscreen: '68.7%',
+        bannerscreen: '75.1%',
+        mobilescreen: '88.4%',
+      },
+    },
+  },
+  plugins: [],
+}
+
